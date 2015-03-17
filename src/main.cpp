ead0d527a699ada4910fb8208be10c82dfd9da6c
Mark Garnett <thecommunistduck@hotmail.co.uk>
2015-03-17 11:54:19 +0000

    Added blitting.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,61 @@
 #include <SWI-cpp.h>
 #include <SWI-Prolog.h>
 #include "libtcod.hpp"
+#include <map>
+#include <string>
+#include <memory>
 
-PREDICATE(initRoot, 4)
+std::map<std::string, std::shared_ptr<TCODConsole>> consoles;
+
+PREDICATE(createConsole, 3)
+{
+	if(consoles.find((char*)A1) == consoles.end())
+	{
+		consoles[(char*)A1] = new TCODConsole((int)A2, (int)A3);
+		return TRUE;
+	}
+	return FALSE;
+}
+
+PREDICATE(blit_console, 10)
+{
+	if(consoles.find((char*)A1) == consoles.end() || consoles.find((char*)A6) == consoles.end())
+		return FALSE;
+	TCODConsole* src = consoles.find((char*)A1)->second;
+	TCODConsole* dst = consoles.find((char*)A6)->second;
+
+	TCODConsole::blit(src, (int)A2, (int)A3, (int)A4, (int)A5, dst, (int)A7, (int)A8, (float)A9, (float)A10);
+	return TRUE;
+}
+
+PREDICATE(destroyConsole, 1)
+{
+	std::map<std::string, std::shared_ptr<TCODConsole>>::iterator it = consoles.find((char*)A1);
+	if(it != consoles.end())
+		consoles.erase(it);
+	return TRUE;
+}
+
+PREDICATE(init_root, 4)
 {
 	TCODConsole::initRoot((int)A1, (int)A2, (char*)A3, (((char*)A4) == "true"));
+	consoles["root"] = TCODConsole::root;
 	return TRUE;
 }
 
 PREDICATE(root_clear, 0)
 {
 	TCODConsole::root->clear();
+	return TRUE;
+}
+
+PREDICATE(putchar, 4)
+{
+	std::map<std::string, std::shared_ptr<TCODConsole>>::iterator it = consoles.find((char*)A1);
+	if(it == consoles.end())
+		return FALSE;
+
+	it->second->putchar((int)A2, (int)A3, ((char*)A4)[0]);
 	return TRUE;
 }
 
@@ -43,3 +88,4 @@ PREDICATE(wait_for_keypress, 1)
 	PlCompound comp = PlCompound("key", PlTermv((long)key.pressed, (long)key.vk, (long)key.c));
 	return A1 = comp;
 }
+
--- tcod.pl
+++ tcod.pl
@@ -10,4 +10,8 @@ input:-
 
 hi :-
 	initRoot(80, 40, 'hi there', true),
+	create_console("bar", 20, 20),
+	putchar("bar", 5, 5, '@'),
+	blit_console("bar", 0, 0, 20, 20, "root", 0, 0, 1.0, 1.0),
+	flush,
 	input.
